973e8010b0542b41a0e95c82b1cc4f3b312f0dd5
vkSegfault <adtofaust@gmail.com>
2025-05-06 16:48:09 +0200

    add seacrh users funtionallti

--- src/components/UserList.vue
+++ src/components/UserList.vue
@@ -8,7 +8,8 @@ import PulseLoader from 'vue-spinner/src/PulseLoader.vue'
 const store = useStore();
 
 const state = reactive({
-    users: []
+    users: [],
+    filteredUsers: []
 });
 
 // store all users in a state variable
@@ -57,6 +58,28 @@ onMounted(async () => {
 
 });
 
+const input = reactive({
+  value: ''
+});
+
+let input2 = ref("");
+
+function filterUsers() {
+    let filteredUsers = state.users.filter((user) =>
+        user.userName.toLowerCase().includes(input.value.toLowerCase())
+        // console.log("User is: " + user.userName)
+    );
+    console.log(input.value)
+
+    console.log("Filtered users: " + filteredUsers[0].userName);
+    console.log("Raw users: " + state.users[0].userName);
+    console.log("There are " + filteredUsers.length + " that matches search criteria");
+
+    state.filteredUsers = filteredUsers;
+
+    return filteredUsers;
+}
+
 </script>
 
 <template>
@@ -64,7 +87,7 @@ onMounted(async () => {
         <div class="container-xl lg:container m-auto">
             <h2 class="text-3xl font-bold text-green-500 mb-6 text-center">
 
-                <form class="max-w-md mx-auto">   
+                <form class="max-w-md mx-auto" @submit.prevent="filterUsers"> 
                     <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
                     <div class="relative">
                         <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
@@ -72,7 +95,7 @@ onMounted(async () => {
                                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
                             </svg>
                         </div>
-                        <input type="search" id="default-search" class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search users..." required />
+                        <input type="search" v-model="input.value" id="default-search" class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search users..." required />
                         <button type="submit" class="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
                     </div>
                 </form>
@@ -87,9 +110,13 @@ onMounted(async () => {
             <ul role="list" class="grid grid-cols-1 divide-y divide-gray-600">
                 <div class="justify-between gap-x-6 py-5">
                     <!-- "asset" is prop from Asset.vue -->
-                    <User v-for="user in state.users" :key="user.userName" :user="user" >  
+                    <!-- <User v-for="user in state.users" :key="user.userName" :user="user" > -->
+                    <!-- <User v-for="user in state.users" :key="user.userName" :user="user" > -->
                         <!-- {{ job.title }} -->
-                    </User>
+                    <!-- </User> -->
+                    <div class="item user" v-for="user in state.filteredUsers">
+                        <User :key="user.userName" :user="user"></User>
+                    </div>
                 </div>
             </ul>
 
